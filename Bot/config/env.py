8f0dbafcc0588107ebe97a6ae3c9b9537f097797
Tran Gia Khanh <90601002+khanhtran0111@users.noreply.github.com>
2024-04-24 09:36:05 +0700

    Add files via upload

--- Bot/config/env.py
+++ Bot/config/env.py
@@ -1,6 +1,7 @@
 from func.example import PingCog
+from func.note import NoteCog
 
-BOT_AUTHORIZE_TOKEN="MTIzMTUzNDY1Mjc4Nzc4NTc3OQ.GeUkOq.00YSAZ8r0ald6xKxitwJaXW0I4J9_aLEbFeows"
+BOT_AUTHORIZE_TOKEN="MTE3MzE5NDY3MzI1OTU2MDk2MA.GXmtGu.p7VzAcwuAYv80shMIkyysFdolrDLuFMYp2LIo8"
 BOT_COMMAND_PREFIX="!"
 
-BOT_CONFIG_LIST = [PingCog]
+BOT_CONFIG_LIST = [PingCog, NoteCog]
--- Bot/func/example.py
+++ Bot/func/example.py
@@ -4,10 +4,10 @@ class PingCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="ping", description="Responds with 'pong'")
+    @commands.command(name="ping", help=":Responds with 'pong'")
     async def ping(self, ctx):
         await ctx.send("pong")
 
-    @commands.command(name="pong", description="Responds with 'ping'")
+    @commands.command(name="pong", help=":Responds with 'ping'")
     async def pong(self, ctx):
         await ctx.send("ping")
