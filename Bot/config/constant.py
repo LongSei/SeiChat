c045f043c8eaef8450b9dbed9f25dbf59062839c
LongSei <74856548+LongSei@users.noreply.github.com>
2024-05-16 22:02:13 +0700

    Add Financial Management (#7)
    * Add Financial Management
    
    * Update Env.py
    
    * Delete Bot/user_notes.json
    
    * Delete Bot/user_tasks.json
    
    * Delete Bot/user_spendings.json
    
    ---------
    
    Co-authored-by: Charlottes84 <hungnguyenduc023@gmail.com>

--- Bot/config/constant.py
+++ Bot/config/constant.py
@@ -12,3 +12,7 @@ CATEGORY_ORDER = ["🔴", "🟡", "🔵", "🟢"]
 GREEN_TICK_EMOJI = "✅"
 RED_CANCEL_EMOJI = "❌"
 SETTING_EMOJI = "🛠️"
+
+# Emoji for SpendingCog
+RECEIVE_MONEY_EMOJI = "💵"
+SPEND_MONEY_EMOJI =  "💸"
