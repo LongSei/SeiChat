8b76bc896d6cdc00d01d9adc34de86a28de7095d
Tran Gia Khanh <90601002+khanhtran0111@users.noreply.github.com>
2024-05-16 21:25:58 +0700

    Add files via upload

--- Bot/config/env.py
+++ Bot/config/env.py
@@ -1,7 +1,9 @@
 from func.example import PingCog
 from func.note import NoteCog
+from func.weather import WeatherCog
+from func.formula1 import F1Cog
 
-BOT_AUTHORIZE_TOKEN="MTIzMTUzNDY1Mjc4Nzc4NTc3OQ.GeUkOq.00YSAZ8r0ald6xKxitwJaXW0I4J9_aLEbFeows"
+BOT_AUTHORIZE_TOKEN="lmao"
 BOT_COMMAND_PREFIX="!"
 
-BOT_CONFIG_LIST = [PingCog, NoteCog]
+BOT_CONFIG_LIST = [PingCog, NoteCog, WeatherCog, F1Cog]
